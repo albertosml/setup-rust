f238a7c77d750c98a6b0845cbcf32b9cac8faa4b
Alberto Silvestre Montes Linares <albertosilvestremonteslinares@gmail.com>
2021-10-17 14:38:05 +0200

    Adding context as input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Set the directory to execute the installation and/or tests.
+context=$1
+
+# Inform to the user about the context directory.
+echo "Going to \"$context\" directory"
+
+# Go to the context directory.
+cd $context
+
 # Inform that we are installing the project.
 echo "Installing the project"
 
@@ -10,7 +19,7 @@ cargo install
 echo "Project installed successfully"
 
 # Run tests if specified.
-run_tests=$1
+run_tests=$2
 
 if [ "$run_tests" = true ] ; then
 	# Inform that the tests are being executed.
